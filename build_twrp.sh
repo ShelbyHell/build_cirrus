6c123e9de44e96c20200c71b72cdf179f3216c17
vsc-sxx <shelbyhell@proton.me>
2022-12-03 05:10:07 +0000

    trigger shrp build.
    Signed-off-by: vsc-sxx <shelbyhell@proton.me>

--- build_twrp.sh
+++ build_twrp.sh
@@ -2,15 +2,14 @@
 ZIPNAME="out/target/product/juice/recovery.img"
 
 echo "TWRP: repo sync"
-curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="TWRP: repo sync"
+curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="SHRP: repo sync"
 mkdir twrp && cd twrp
-repo init --depth=1 --no-repo-verify -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SHRP/manifest.git -b v3_9.0 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-echo "TWRP: cloning device tree & build twrp."
+curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="SHRP: cloning device tree && build twrp."
 git clone https://github.com/vsc-sxx/device_xiaomi_juice-twrp device/xiaomi/juice
 source build/envsetup.sh
 lunch twrp_juice-eng
 mka recoveryimage ALLOW_MISSING_DEPENDENCIES=true # Only if you use minimal twrp tree.
-rclone copy $ZIPNAME shelby:twrp -P
-curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="TWRP 3.7.0 | Branch: twrp-12.1 | STATUS: TESTING | Device: juice
-Owner please check your gdrive."
+rclone copy $ZIPNAME shelby:shrp -P
+curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="SHRP | Branch: shrp-12.1 | STATUS: TESTING | Device: juice"
