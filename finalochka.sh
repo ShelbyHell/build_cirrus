997fe423fdb53560f9ae2559eb4f58a26cc428ca
Alexey Shelby <lexaxx14@gmail.com>
2023-01-28 23:14:25 +0300

    Update finalochka.sh

--- finalochka.sh
+++ finalochka.sh
@@ -7,7 +7,7 @@ cd /tmp
 ###################
 
 # Sync ROM(Cherish)
-cd /tmp/rom
+mkdir rom && cd /tmp/rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ShelbyHell/local_manifest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
